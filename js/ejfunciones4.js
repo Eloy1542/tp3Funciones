0af891c73984cf46d3f3f84a7a435c17ae0f303b
Eloy Pintos <eloy_pintos14@hotmail.com>
2022-07-24 17:01:04 -0300

    ejercicio 4 de funciones terminado

---
+++ js/ejfunciones4.js
@@ -0,0 +1,17 @@
+let numeroIngresado = parseInt(prompt("Ingrese un numero"));
+
+tabla(numeroIngresado);
+
+function tabla(numero) {
+  document.write(`<table>
+    <tbody>`);
+
+  for (i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    document.write(`<tr>`);
+    
+    document.write(`<td> ${resultado} </td>`);
+  }
+}
+document.write(`</tbody>
+</table>`);
